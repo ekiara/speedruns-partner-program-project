7c8227d7e9770f0fff75befc350ff2874709b777
Eric Kiara <eric@kiara.co.ke>
2019-11-21 09:50:23 +0300

    Update project urls

--- project_api/project_api/urls.py
+++ project_api/project_api/urls.py
@@ -6,9 +6,9 @@ from django.urls import re_path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    re_path('^api/v1/$', RedirectView.as_view(url='/api/v1/partners/', permanent=False)),
-    re_path('^api/$', RedirectView.as_view(url='/api/v1/partners/', permanent=False)),
-    re_path('^$', RedirectView.as_view(url='/api/v1/partners/', permanent=False)),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("api.urls")),
+    re_path(route="^api/v1/$", view=RedirectView.as_view(url="/api/v1/partners/", permanent=False)),
+    re_path(route="^api/$", view=RedirectView.as_view(url="/api/v1/partners/", permanent=False)),
+    re_path(route="^$", view=RedirectView.as_view(url="/api/v1/partners/", permanent=False)),
 ]
