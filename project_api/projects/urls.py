ecddec43c8064a28050f7148142a5cab8da6abd5
Eric Kiara <eric@kiara.co.ke>
2019-11-21 09:49:10 +0300

    Update projects urls

--- project_api/projects/urls.py
+++ project_api/projects/urls.py
@@ -8,9 +8,9 @@ from .views import ProjectUpdate
 from .views import ProjectDelete
 
 urlpatterns = [
-    path('', ProjectListView.as_view()),
-    path('create/', ProjectCreate.as_view()),
-    path('<int:pk>/', ProjectRetrieve.as_view()),
-    path('<int:pk>/update/', ProjectUpdate.as_view()),
-    path('<int:pk>/delete/', ProjectDelete.as_view()),
+    path(route='', view=ProjectListView.as_view(), name="projects_root"),
+    path(route='create/', view=ProjectCreate.as_view(), name="projects_create"),
+    path(route='<int:pk>/', view=ProjectRetrieve.as_view(), name="projects_retrieve"),
+    path(route='<int:pk>/update/', view=ProjectUpdate.as_view(), name="projects_update"),
+    path(route='<int:pk>/delete/', view=ProjectDelete.as_view(), name="projects_delete"),
 ]
