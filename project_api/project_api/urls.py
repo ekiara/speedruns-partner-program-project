c6c6457f6c129a59ee5d6ef53216e061435fa35b
Eric Kiara <eric@kiara.co.ke>
2019-11-21 06:19:15 +0300

    Update project URLCONF
    - More friendly URL redirecting when a client visits a bare /api/v1/ URL

--- project_api/project_api/urls.py
+++ project_api/project_api/urls.py
@@ -2,8 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.urls import re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    re_path('^api/v1/$', RedirectView.as_view(url='/api/v1/partners/', permanent=False)),
 ]
