df5fa832f25d138924aba39c09cc0b927b086f2c
Eric Kiara <eric@kiara.co.ke>
2019-11-21 06:21:44 +0300

    Update project URLCONF
    - More friendly URL redirecting when a client visits a bare URLs:
        * "/api/" redirects to "/api/vi/partners/"
        * "/" redirects to "/api/vi/partners/"

--- project_api/project_api/urls.py
+++ project_api/project_api/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     re_path('^api/v1/$', RedirectView.as_view(url='/api/v1/partners/', permanent=False)),
+    re_path('^api/$', RedirectView.as_view(url='/api/v1/partners/', permanent=False)),
+    re_path('^$', RedirectView.as_view(url='/api/v1/partners/', permanent=False)),
 ]
