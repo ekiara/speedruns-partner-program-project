73e14f080ed00b197091ce2ba10af069032a4ceb
Eric Kiara <eric@kiara.co.ke>
2019-11-21 09:57:54 +0300

    Update project to use MySQL Database
    - Add my.cnf configuration file
    - Update project conf

--- project_api/project_api/settings.py
+++ project_api/project_api/settings.py
@@ -66,12 +66,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project_api.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {                                                                   
+    'default': {                                                                
+        'ENGINE': 'django.db.backends.mysql',                                   
+        'OPTIONS': {                                                            
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),              
+        },                                                                      
+    }                                                                           
+}                                                                               
 
 AUTH_PASSWORD_VALIDATORS = [
     {
